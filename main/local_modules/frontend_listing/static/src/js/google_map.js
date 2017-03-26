c1204fc1a39272c4fe060a79307654d3e0157c27
Jordi Riera <kender.jr@gmail.com>
2017-03-26 01:12:01 -0400

    frontend_listing: fix locate me in google_map.js
    * add namespace (need to keep on the effort on namespace)
    * use google_map.map instead of local map variable in initialize
    * set google_map.getLocation (use getCurrentPosition instead of
    watchPosition)

--- main/local_modules/frontend_listing/static/src/js/google_map.js
+++ main/local_modules/frontend_listing/static/src/js/google_map.js
@@ -6,6 +6,25 @@
  * CUSTOMS infos windows ref : http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html
  */
 
+// namespace
+var google_map = {
+    map: null,
+
+    /*
+     Get the current position of the user and move the google map to the location
+     */
+    getLocation: function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                google_map.map.panTo(center)
+            });
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+};
+
 function initialize(geoloc) {
     //To center map on markers
     var bounds = new google.maps.LatLngBounds();
@@ -29,7 +48,7 @@ function initialize(geoloc) {
             position: google.maps.ControlPosition.LEFT_CENTER
         }
     };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+    google_map.map = new google.maps.Map(mapCanvas, mapOptions);
     var markers = [];
     var icon = '/frontend_listing/static/src/marker.svg';
     var iconStudio = '/frontend_listing/static/src/marker-studio.svg';
@@ -43,24 +62,23 @@ function initialize(geoloc) {
     });
 
 
-
     jQuery.each(geoloc, function (i, val) {
         var contentString = '<div id="content">' + val[2] + '</div>';
 
         var marker = new google.maps.Marker({
             position: {lat: val[0], lng: val[1]},
-            map: map,
+            map: google_map.map,
             icon: iconStudio,
             title: i
         });
-        
+
         //extend the bounds to include each marker's position
         bounds.extend(marker.position);
         markers.push(marker);
 
         google.maps.event.addListener(marker, 'click', function () {
             infowindow.setContent(contentString);
-            infowindow.open(map, marker);
+            infowindow.open(google_map.map, marker);
         });
 
     });
@@ -81,34 +99,12 @@ function initialize(geoloc) {
         maxZoom: 15
     };
 
-    var mc = new MarkerClusterer(map, markers, mcOptions);
+    var mc = new MarkerClusterer(google_map.map, markers, mcOptions);
     //now fit the map to the newly inclusive bounds
-    map.fitBounds(bounds);
-    map.setOptions({styles: snazzy_theme()});
-	
-	
+    google_map.map.fitBounds(bounds);
+    google_map.map.setOptions({styles: snazzy_theme()});
+
+
 }
 
-
-function getLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.watchPosition(showPosition);
-	} else {
-		x.innerHTML = "Geolocation is not supported by this browser.";
-	}
-}
-
-function showPosition(position) {
-	var latx = position.coords.latitude;
-	var lonx = position.coords.longitude;
-	var resultx = new google.maps.LatLng(latx, lonx);
-	var userLocations = [['Your location', position.coords.latitude, position.coords.longitude], ];
-
-
-	var mapCanvasCustom = new google.maps.Map(document.getElementById('map'), {
-		zoom: 6,
-		center: resultx
-	});
-}
 google.maps.event.addDomListener(window, 'load', initialize);
-
